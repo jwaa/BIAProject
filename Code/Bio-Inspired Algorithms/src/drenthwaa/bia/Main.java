dc51eb18eb4b8c218580aeccb621e191a092073e
JSWaa <jaspervanderwaa@gmail.com>
2014-03-25 15:25:23 +0100

    + Added a DataManager to the main to collect the data from the basic experiment

--- Code/Bio-Inspired Algorithms/src/drenthwaa/bia/Main.java
+++ Code/Bio-Inspired Algorithms/src/drenthwaa/bia/Main.java
@@ -4,6 +4,7 @@ import drenthwaa.bia.optainet.OAConfig;
 import drenthwaa.bia.testing.Experiment;
 import drenthwaa.bia.testing.TestingParameters;
 import drenthwaa.bia.testing.cell.RandomCellGenerator;
+import drenthwaa.bia.testing.data.DataManager;
 import drenthwaa.bia.testing.function.ExampleFunction;
 
 public class Main
@@ -13,11 +14,12 @@ public class Main
 		OAConfig config = OAConfig.readConfigFile("OptAinet_config.txt");
 		
 		TestingParameters basicParam = new TestingParameters();
+		DataManager dataManager = new DataManager(basicParam);
 		basicParam.affinityMeasure = TestingParameters.AFFINITY_EUCLIDEAN;
 		basicParam.optimisationFunction = ExampleFunction.getInstance();
 		basicParam.cellGenerator = RandomCellGenerator.getInstance();
 		
-		Experiment basicExperiment = new Experiment(config, basicParam);
+		Experiment basicExperiment = new Experiment(config, basicParam, dataManager);
 		basicExperiment.executeExperiment();
 		
 		/*
